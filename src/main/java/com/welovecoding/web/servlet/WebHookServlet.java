945f29f6e759e6669541fe45df5514d6491107f5
Benny Neugebauer <bn@bennyn.de>
2014-08-19 00:02:52 +0200

    GitHub trigger

---
+++ src/main/java/com/welovecoding/web/servlet/WebHookServlet.java
@@ -0,0 +1,45 @@
+package com.welovecoding.web.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "WebHookServlet", urlPatterns = {"/webhook/*"})
+public class WebHookServlet extends HttpServlet {
+
+  protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    response.setContentType("text/html;charset=UTF-8");
+
+    try (PrintWriter out = response.getWriter()) {
+      out.println("<!DOCTYPE html>");
+      out.println("<html>");
+      out.println("<head>");
+      out.println("<title>WebHookServlet</title>");
+      out.println("</head>");
+      out.println("<body>");
+      out.println("<p>OK</p>");
+      out.println("</body>");
+      out.println("</html>");
+    }
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest servletRequest, HttpServletResponse response)
+          throws ServletException, IOException {
+    String userAgent = servletRequest.getHeader("user-agent");
+    String event = servletRequest.getHeader("x-github-event");
+
+    if (userAgent.contains("GitHub-Hookshot") && event.equals("push")) {
+      String payload = servletRequest.getParameter("payload");
+      System.out.println("GITHUB PAYLOAD: " + payload);
+    }
+
+    processRequest(servletRequest, response);
+  }
+
+}
