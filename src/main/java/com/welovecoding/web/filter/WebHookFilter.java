b7c4456fab7bba69127ddad4b741924f8938313f
Benny Neugebauer <bn@bennyn.de>
2014-08-18 23:41:40 +0200

    Added commit hook.

---
+++ src/main/java/com/welovecoding/web/filter/WebHookFilter.java
@@ -0,0 +1,39 @@
+package com.welovecoding.web.filter;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter("/webhook/*")
+public class WebHookFilter implements Filter {
+
+  private static final Logger LOG = Logger.getLogger(WebHookFilter.class.getName());
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    HttpServletRequest servletRequest = (HttpServletRequest) request;
+    HttpServletResponse servletResponse = (HttpServletResponse) response;
+
+    LOG.log(Level.INFO, "Host: {0}", servletRequest.getServerName());
+
+    chain.doFilter(request, response);
+  }
+
+  @Override
+  public void init(FilterConfig config) throws ServletException {
+  }
+
+  @Override
+  public void destroy() {
+  }
+
+}
