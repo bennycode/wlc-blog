b0ebb32f94dd67db1fc12dd0534f7a0a81c54596
Benny Neugebauer <bn@bennyn.de>
2014-08-20 10:14:34 +0200

    I love clean code.

--- src/main/java/com/welovecoding/web/test/Test.java
+++ src/main/java/com/welovecoding/web/test/Test.java
@@ -10,19 +10,20 @@ public class Test {
   private static final String REMOTE_URL = "https://github.com/bennyn/wlc-blog";
 
   public static void main(String[] args) throws Exception {
-    File localPath = File.createTempFile("TestGitRepository", "");
-    localPath.delete();
+    String homeDirectory = detectHomeDirectory();
+    String repositoryName = parseRepositoryName(REMOTE_URL);
+    File checkoutPath = createRepositoryCheckoutPath(homeDirectory, repositoryName);
 
     // then clone
-    System.out.println("Cloning from " + REMOTE_URL + " to " + localPath);
+    System.out.println("Cloning from " + REMOTE_URL + " to " + checkoutPath);
     Git.cloneRepository()
             .setURI(REMOTE_URL)
-            .setDirectory(localPath)
+            .setDirectory(checkoutPath)
             .call();
 
     // now open the created repository
     FileRepositoryBuilder builder = new FileRepositoryBuilder();
-    Repository repository = builder.setGitDir(localPath)
+    Repository repository = builder.setGitDir(checkoutPath)
             .readEnvironment() // scan environment GIT_* variables
             .findGitDir() // scan up the file system tree
             .build();
@@ -30,6 +31,28 @@ public class Test {
     System.out.println("Having repository: " + repository.getDirectory());
 
     repository.close();
+  }
+
+  private static String detectHomeDirectory() {
+    return System.getProperty("user.home");
+  }
+
+  private static String parseRepositoryName(String name) {
+    int beginIndex = name.lastIndexOf("/");
+    return name.substring(beginIndex);
+  }
+
+  /**
+   * ...
+   *
+   * @param home Path of the root directory for the checkout (Example:
+   * C:\Users\Benny)
+   * @param name Name of the Repository (Example: wlc-blog)
+   * @return Representation of the checkout path (Example:
+   * C:\Users\Benny\wlc-blog)
+   */
+  private static File createRepositoryCheckoutPath(String home, String name) {
+    return new File(home + System.getProperty("file.separator") + name);
   }
 
 }
