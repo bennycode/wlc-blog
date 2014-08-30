b62591a23b2756fa480f56aba581927222ab911d
Benny Neugebauer <bn@bennyn.de>
2014-08-30 22:05:58 +0200

    Added Test.

---
+++ src/test/java/com/welovecoding/util/markdown/meta/MarkdownMetaParserTest.java
@@ -0,0 +1,38 @@
+package com.welovecoding.util.markdown.meta;
+
+import java.io.InputStream;
+import java.util.Map;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MarkdownMetaParserTest {
+
+  public MarkdownMetaParserTest() {
+  }
+
+  @BeforeClass
+  public static void setUpClass() {
+  }
+
+  @AfterClass
+  public static void tearDownClass() {
+  }
+
+  @Before
+  public void setUp() {
+  }
+
+  @After
+  public void tearDown() {
+  }
+
+  @Test
+  public void testParseStream() {
+    assertEquals("A", "A");
+  }
+
+}
