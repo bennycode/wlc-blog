1c57ded31729c9f43b8dfcd1240448bedab6c4b9
Benny Neugebauer <bn@bennyn.de>
2014-09-14 21:06:56 +0200

    large texts

--- src/main/java/com/welovecoding/web/blog/domain/article/Article.java
+++ src/main/java/com/welovecoding/web/blog/domain/article/Article.java
@@ -12,6 +12,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Temporal;
@@ -35,6 +36,7 @@ public class Article implements Serializable {
 
   private String title;
   private String description;
+  @Lob
   private String html;
   private List<String> tags;
 
